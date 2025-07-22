08ae368822b4ca76f67e5d2eb9bc5acfd1246b07
juwonss <bestjuwon@gmail.com>
2025-07-22 17:19:32 +0900

    문구 수정 - 재배포용

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          호이짜~
-          강성이형 메롱~~~ㅋㅋㅋㅋㅋ
+          강성이형메롱 이번엔 진짜배포한다
         </p>
         <a
           className="App-link"
