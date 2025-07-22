19d398311234ff33072bb48ee3ce5d1e6c540907
juwonss <bestjuwon@gmail.com>
2025-07-22 17:02:17 +0900

    첫 화면 수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          강성이형 메롱~~~ㅋㅋㅋㅋㅋ
         </p>
         <a
           className="App-link"
@@ -21,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
